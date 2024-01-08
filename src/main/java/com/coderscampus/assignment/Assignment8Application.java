6fd0d6a8769c35b4c20b725c48c61516bbf1502f
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-08 10:56:39 -0800

    Update Assignment8Application.java

--- src/main/java/com/coderscampus/assignment/Assignment8Application.java
+++ src/main/java/com/coderscampus/assignment/Assignment8Application.java
@@ -1,11 +1,10 @@
 package com.coderscampus.assignment;
 
-
-
 public class Assignment8Application {
 
 	public static void main(String[] args) {
 		SortingService sort = new SortingService();
+		
 		sort.fetchAllNumbers();
 		sort.combineListsAndFindNumberOfDuplicates();
 
