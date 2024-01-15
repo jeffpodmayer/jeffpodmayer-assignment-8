edf7248109b108d6a5052f8466463e30829e6a73
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-15 10:16:54 -0800

    Updated with reviewer feedback

--- src/main/java/com/coderscampus/assignment/Assignment8Application.java
+++ src/main/java/com/coderscampus/assignment/Assignment8Application.java
@@ -3,10 +3,11 @@ package com.coderscampus.assignment;
 public class Assignment8Application {
 
 	public static void main(String[] args) {
-		SortingService sort = new SortingService();
+		Assignment8Service sort = new Assignment8Service();
 		
 		sort.fetchAllNumbers();
-		sort.combineListsAndFindNumberOfDuplicates();
+		sort.countNumberFrequency();
+		sort.displayNumbersCountMap();
 
 	}
 }
--- src/main/java/com/coderscampus/assignment/Assignment8Service.java
+++ src/main/java/com/coderscampus/assignment/Assignment8Service.java
@@ -9,12 +9,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class SortingService {
-	ExecutorService cachedService = Executors.newCachedThreadPool();
+public class Assignment8Service {
+	private ExecutorService cachedService = Executors.newCachedThreadPool();
 
-	List<CompletableFuture<List<Integer>>> numbers = new ArrayList<>();
-	List<Integer> combinedList = new ArrayList<>();
-	Map<Integer, AtomicInteger> numberCounts = new ConcurrentHashMap<>();
+	private List<CompletableFuture<List<Integer>>> numbers = new ArrayList<>();
+	private List<Integer> combinedList = new ArrayList<>();
+	private Map<Integer, AtomicInteger> numberCounts = new ConcurrentHashMap<>();
 
 	Assignment8 assignment = new Assignment8();
 
@@ -28,21 +28,24 @@ public class SortingService {
 		
 		System.out.println("Number of completed threads: " + numbers.stream().filter(CompletableFuture::isDone).count());
 		cachedService.shutdown();
-		
 	}
 
 	
-	public void combineListsAndFindNumberOfDuplicates() {
+	public void countNumberFrequency() {
 		combinedList = numbers.stream().map(CompletableFuture::join).flatMap(List::stream).toList();
 
 		for (Integer number : combinedList) {
 			numberCounts.putIfAbsent(number, new AtomicInteger(0));
 			numberCounts.get(number).incrementAndGet();	
 		}
+		
+	}
 
+	public void displayNumbersCountMap() {
 		for (int i = 0; i < 15; i++) {
 			Integer count = numberCounts.getOrDefault(i, new AtomicInteger(0)).get();
 			System.out.println("The number " + i + " shows up " + count + " times!");
+		
 		}
 
 	}
